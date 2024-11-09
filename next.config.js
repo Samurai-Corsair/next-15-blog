bc63c246dc42ed5ac535fdbd671a2dc03f745bdf
corsair <tony.corsair.corp@gmail.com>
2024-11-09 23:02:10 +0200

    Add topic-creation form + validation

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
     env: {
         GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
         GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
         AUTH_SECRET: process.env.AUTH_SECRET,
     },
-};
+}
 
-export default nextConfig;
+module.exports = nextConfig
--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,37 @@
 "use server";
 
-export async function createTopic() {
-    //todo revalidate a homepage after creating a topic
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, "must be lowercase letters or dashes without spaces"),
+  description: z.string().min(5),
+});
+
+interface CreateTopicFormState {
+  errors: {
+    name?: string[];
+    description?: string[];
+  };
+}
+
+export async function createTopic(
+  prevState: CreateTopicFormState,
+  formData: FormData,
+): Promise<CreateTopicFormState> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+  if (!result.success) {
+    return {
+      errors: result.error.flatten().fieldErrors,
+    };
+  }
+  return {
+    errors: {},
+  };
+  //todo revalidate a homepage after creating a topic
 }
